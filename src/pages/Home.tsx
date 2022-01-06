89bd6ee74fd550af4b955540626d491be02133c5
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-06 10:26:35 -0300

    ✨ Doing all the tasks for the test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,27 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => {
+      if(task.id == id)
+        task.done = task.done == true ? false : true;
+      return task;
+    })
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id != id));
   }
 
   return (
